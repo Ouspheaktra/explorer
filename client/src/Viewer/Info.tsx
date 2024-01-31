4f1473e7711f2919cb8a6fb0347f2a8fa3350cb5
Ouspheaktra <ouspheaktraphal@gmail.com>
2024-02-01 01:32:21 +0700

    Add type string in Info

--- client/src/Viewer/Info.tsx
+++ client/src/Viewer/Info.tsx
@@ -29,60 +29,78 @@ export default function Info<iDetails extends object>({
           name: string;
           type: DetailsType;
         };
-        if (type === "string[]") {
-          const pluralName = name;
-          if (name.endsWith("s")) name = name.slice(0, -1);
-          const data: string[] = file.details[pluralName] || [];
-          return (
-            <div
-              key={pluralName}
-              className={`string[]-wrapper ${pluralName}-wrapper`}
-            >
-              {data.map((one) => (
-                <span key={one} className={`string ${name}`}>
-                  <button
-                    className="x"
-                    onClick={() =>
-                      update({
-                        [pluralName]: data.filter((a) => a !== one),
-                      })
-                    }
-                  >
-                    &times;
-                  </button>
-                  {one}
-                </span>
-              ))}
-              <input
-                className={`string-input ${name}-input`}
-                placeholder={name}
-                list={`info-${pluralName}`}
-                onKeyUp={(e) => {
-                  if (e.key === "Enter" && e.currentTarget.value.trim()) {
-                    update({
-                      [pluralName]: [
-                        ...data,
-                        e.currentTarget.value.trim(),
-                      ].sort(),
-                    });
-                    e.currentTarget.value = "";
-                  }
-                }}
-              />
-              <datalist id={`info-${pluralName}`}>
-                {[
-                  ...new Set(
-                    files.map((file) => file.details[pluralName] || []).flat()
-                  ),
-                ].map((one) => (
-                  <option key={one}>{one}</option>
-                ))}
-              </datalist>
-            </div>
-          );
-        }
-        // else if (type === "string")
-        else return "no type = " + type;
+        return (
+          <div key={name} className={`${type}-wrapper ${name}-wrapper`}>
+            {(() => {
+              if (type === "string[]") {
+                const pluralName = name;
+                if (name.endsWith("s")) name = name.slice(0, -1);
+                const data: string[] = file.details[pluralName] || [];
+                return (
+                  <>
+                    {data.map((one) => (
+                      <span key={one} className={`${type} ${name}`}>
+                        <button
+                          className="x"
+                          onClick={() =>
+                            update({
+                              [pluralName]: data.filter((a) => a !== one),
+                            })
+                          }
+                        >
+                          &times;
+                        </button>
+                        {one}
+                      </span>
+                    ))}
+                    <input
+                      className={`${type}-input ${name}-input`}
+                      placeholder={name}
+                      list={`info-${pluralName}`}
+                      onKeyUp={(e) => {
+                        if (e.key === "Enter" && e.currentTarget.value.trim()) {
+                          update({
+                            [pluralName]: [
+                              ...data,
+                              e.currentTarget.value.trim(),
+                            ].sort(),
+                          });
+                          e.currentTarget.value = "";
+                        }
+                      }}
+                    />
+                    <datalist id={`info-${pluralName}`}>
+                      {[
+                        ...new Set(
+                          files
+                            .map((file) => file.details[pluralName] || [])
+                            .flat()
+                        ),
+                      ].map((one) => (
+                        <option key={one}>{one}</option>
+                      ))}
+                    </datalist>
+                  </>
+                );
+              } else if (type === "string") {
+                return (
+                  <input
+                    className={`${type}-input ${name}-input`}
+                    defaultValue={file.details[name] || ""}
+                    placeholder={name}
+                    onKeyUp={(e) => {
+                      if (e.key === "Enter" && e.currentTarget.value.trim()) {
+                        update({
+                          [name]: e.currentTarget.value.trim(),
+                        });
+                      }
+                    }}
+                  />
+                );
+              } else return "no type = " + type;
+            })()}
+          </div>
+        );
       })}
     </div>
   );
