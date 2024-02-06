890a8dc2a1b538f99ee5faeb2bb9ead6e37add90
Ouspheaktra <ouspheaktraphal@gmail.com>
2024-02-06 23:07:32 +0700

    Upgrade List

--- client/src/Explorer.tsx
+++ client/src/Explorer.tsx
@@ -2,37 +2,17 @@ import { useState } from "react";
 import List from "./List";
 import { useGlobal } from "./contexts/GlobalContext";
 
-type OrderName = "name" | "date";
-type OrderOrder = "asc" | "desc";
-type Order = [OrderName, OrderOrder];
-
 export default function Explorer() {
-  const {
-    dir: { prevDir, files },
-    goto,
-    file,
-    setFile,
-    setViewerMode,
-  } = useGlobal();
-  const [[orderName, orderOrder], setOrder] = useState<Order>(["name", "asc"]);
-  const orderedFiles = [...files];
-  if (orderName === "name") {
-    if (orderOrder === "asc")
-      orderedFiles.sort((a, b) => a.name.localeCompare(b.name));
-    else orderedFiles.sort((a, b) => b.name.localeCompare(a.name));
-  } else if (orderName === "date") {
-    if (orderOrder === "asc")
-      orderedFiles.sort(
-        (a, b) => a.stat!.mtime.getTime() - b.stat!.mtime.getTime()
-      );
-    else
-      orderedFiles.sort(
-        (a, b) => b.stat!.mtime.getTime() - a.stat!.mtime.getTime()
-      );
-  }
+  // const {
+  //   dir,
+  //   goto,
+  //   file,
+  //   setFile,
+  //   setViewerMode,
+  // } = useGlobal();
   return (
     <List>
-      {file && (
+      {/* {file && (
         <button
           onClick={() => setViewerMode(true)}
           style={{
@@ -57,55 +37,7 @@ export default function Explorer() {
           &lt;&minus; Back
         </li>
       )}
-      {file && <li onClick={() => setFile(null)}>===</li>}
-      {orderedFiles.map((f, fid) => {
-        const { type, ext, name, dir, _id } = f;
-        const isCurrent = file && file._id === _id;
-        return (
-          <li
-            key={fid}
-            className={`is-${type} ${isCurrent ? "active" : ""}`}
-            onClick={
-              type !== "unknown" && !isCurrent
-                ? () => {
-                    // is file, change file
-                    if (ext) setFile(f);
-                    // if directory, change dir
-                    else goto(dir);
-                  }
-                : undefined
-            }
-          >
-            {name}
-            {ext}
-          </li>
-        );
-      })}
-      <li
-        style={{
-          position: "sticky",
-          bottom: 0,
-          background: "black",
-        }}
-      >
-        Order:
-        <ul>
-          {["name", "date"].map((o) => (
-            <button
-              key={o}
-              style={{ background: orderName === o ? "yellow" : "" }}
-              onClick={() =>
-                setOrder([
-                  o as OrderName,
-                  orderOrder === "asc" ? "desc" : "asc",
-                ])
-              }
-            >
-              by {o}
-            </button>
-          ))}
-        </ul>
-      </li>
+      {file && <li onClick={() => setFile(null)}>===</li>} */}
     </List>
   );
 }
--- client/src/List.tsx
+++ client/src/List.tsx
@@ -1,17 +1,99 @@
-import { HTMLProps, useState } from "react";
+import { HTMLProps, ReactNode, useState } from "react";
 import "./List.scss";
+import { useGlobal } from "./contexts/GlobalContext";
+import { iFile } from "./types";
 
-export default function List({
-  children,
-  ...props
-}: HTMLProps<HTMLUListElement>) {
+type Order = "asc" | "desc";
+type Sort = {
+  name: string;
+  sort: (files: iFile[], order: Order) => iFile[];
+};
+
+const builtinSorts: Sort[] = [
+  {
+    name: "Name",
+    sort: (files, order) => {
+      if (order === "asc") files.sort((a, b) => a.name.localeCompare(b.name));
+      else files.sort((a, b) => b.name.localeCompare(a.name));
+      return files;
+    },
+  },
+  {
+    name: "Date",
+    sort: (files, order) => {
+      if (order === "asc")
+        files.sort((a, b) => a.stat!.mtime.getTime() - b.stat!.mtime.getTime());
+      else
+        files.sort((a, b) => b.stat!.mtime.getTime() - a.stat!.mtime.getTime());
+      return files;
+    },
+  },
+];
+
+export default function List({ sorts = [] }: { sorts?: Sort[] }) {
   const [open, setOpen] = useState(true);
+  const {
+    dir: { files },
+    goto,
+    file,
+    setFile,
+  } = useGlobal();
+  const [[sortName, sortOrder], setSort] = useState<[string, Order]>([
+    builtinSorts[0].name,
+    "asc",
+  ]);
+  const allSorts = [...sorts, ...builtinSorts];
+  const sortedFiles = allSorts
+    .find((sort) => sort.name === sortName)!
+    .sort([...files], sortOrder);
   return (
-    <ul {...props} className={"list" + (open ? " active" : "")}>
+    <ul className={"list" + (open ? " active" : "")}>
       <button className="list-opener" onClick={() => setOpen(!open)}>
         {open ? "X" : "O"}
       </button>
-      {open && children}
+      {sortedFiles.map((f, fid) => {
+        const { type, ext, name, dir, _id } = f;
+        const isCurrent = file && file._id === _id;
+        return (
+          <li
+            key={fid}
+            className={`is-${type} ${isCurrent ? "active" : ""}`}
+            onClick={
+              type !== "unknown" && !isCurrent
+                ? () => {
+                    // is file, change file
+                    if (ext) setFile(f);
+                    // if directory, change dir
+                    else goto(dir);
+                  }
+                : undefined
+            }
+          >
+            {name}
+            {ext}
+          </li>
+        );
+      })}
+      <li
+        style={{
+          position: "sticky",
+          bottom: 0,
+          background: "black",
+        }}
+      >
+        Order:
+        <ul>
+          {allSorts.map(({ name }) => (
+            <button
+              key={name}
+              style={{ background: sortName === name ? "yellow" : "" }}
+              onClick={() => setSort([name, sortOrder === "asc" ? "desc" : "asc"])}
+            >
+              by {name}
+            </button>
+          ))}
+        </ul>
+      </li>
     </ul>
   );
 }
