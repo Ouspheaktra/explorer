b667b44015e466abac63e6b206bb28465c98d7b6
Ouspheaktra <ouspheaktraphal@gmail.com>
2024-02-08 01:03:46 +0700

    Add fullmode

--- client/src/List/index.tsx
+++ client/src/List/index.tsx
@@ -4,8 +4,15 @@ import { Order, Sort } from "./types";
 import { builtinSorts } from "./builtin";
 import "./style.scss";
 
-export default function List({ listTop, sorts = [] }: { listTop?: ReactNode, sorts?: Sort[] }) {
+export default function List({
+  listTop,
+  sorts = [],
+}: {
+  listTop?: ReactNode;
+  sorts?: Sort[];
+}) {
   const [open, setOpen] = useState(true);
+  const [fullMode, setFullMode] = useState(false);
   const {
     dir: { files },
     setDir: goto,
@@ -17,22 +24,39 @@ export default function List({ listTop, sorts = [] }: { listTop?: ReactNode, sor
     "asc",
   ]);
   const allSorts = [...sorts, ...builtinSorts];
-  const sorter = allSorts.find((sort) => sort.name === sortName)!
+  const sorter = allSorts.find((sort) => sort.name === sortName)!;
   const sortedGroups = sorter.sort([...files], sortOrder);
   return (
-    <ul className={"list" + (open ? " active" : "")}>
-      <button className="list-opener" onClick={() => setOpen(!open)}>
-        {open ? "X" : "O"}
-      </button>
+    <ul
+      className={"list" + (open ? " active" : "") + (fullMode ? " full" : "")}
+    >
       <li className="list-top">
         <ul>
+          <li className="list-top-buttons">
+            <button
+              className="list-fuller"
+              onClick={() => setFullMode(!fullMode)}
+              style={{ backgroundColor: "aqua" }}
+            >
+              {fullMode ? "S" : "M"}
+            </button>
+            <button
+              className="list-opener"
+              onClick={() => setOpen(!open)}
+              style={{ backgroundColor: "pink" }}
+            >
+              {open ? "X" : "O"}
+            </button>
+          </li>
           {listTop}
         </ul>
       </li>
       {sortedGroups.map(({ name, files }, gid) => {
         return (
           <li key={gid} className="list-group">
-            {sorter.showLabel && <span className="list-group-name">{name}</span>}
+            {sorter.showLabel && (
+              <span className="list-group-name">{name}</span>
+            )}
             <ul className="list-group-files">
               {files.map((f, fid) => {
                 const { type, ext, name, dir, _id } = f;
@@ -80,4 +104,3 @@ export default function List({ listTop, sorts = [] }: { listTop?: ReactNode, sor
     </ul>
   );
 }
-
