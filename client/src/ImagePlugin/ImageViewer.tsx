1f26006d8545da8f51ac77fff1a1460d2f142ddd
Ouspheaktra <ouspheaktraphal@gmail.com>
2024-02-11 23:43:13 +0700

    Add before and after view

--- client/src/ImagePlugin/ImageViewer.tsx
+++ client/src/ImagePlugin/ImageViewer.tsx
@@ -1,19 +1,51 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import panzoom, { PanZoom } from "panzoom";
 import { useGlobal } from "../GlobalContext";
 import { fileUrl } from "../utils";
 
 export default function ImageViewer() {
   const {
-    file: { _id, path },
+    file: { _id, path, dir, details },
   } = useGlobal();
-  const imageRef = useRef(null);
+  const [editedId, setEditedId] = useState(0);
+  const imageRef = useRef<HTMLImageElement>(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
   const panzoomHandle = useRef<PanZoom>();
   useEffect(() => {
     if (panzoomHandle.current) panzoomHandle.current.dispose();
-    panzoomHandle.current = panzoom(imageRef.current!, {
+    panzoomHandle.current = panzoom(wrapperRef.current!, {
       smoothScroll: false,
     });
   }, [_id]);
-  return <img id="image" ref={imageRef} src={fileUrl(path)} />;
+  const editeds: string[] = details.editeds || [];
+  return (
+    <div
+      id="image"
+      ref={wrapperRef}
+      onClick={
+        editeds.length
+          ? (e) => e.button === 1 && setEditedId(editedId + 1)
+          : undefined
+      }
+      onMouseMove={
+        editeds.length
+          ? (e) => {
+              const { x: imgX, width } =
+                imageRef.current!.getBoundingClientRect();
+              const offsetX = e.clientX - imgX;
+              const x = (offsetX / width) * 100;
+              imageRef.current!.style.clipPath = `polygon(0 0, ${x}% 0, ${x}% 100%, 0 100%)`;
+            }
+          : undefined
+      }
+    >
+      {editeds.length ? (
+        <img
+          className="edited"
+          src={fileUrl(dir + "/" + editeds[editedId % editeds.length])}
+        />
+      ) : null}
+      <img ref={imageRef} src={fileUrl(path)} />
+    </div>
+  );
 }
