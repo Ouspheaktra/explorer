ec95af6b70f528eb530a9d21adf6fbc65c33cf23
Ouspheaktra <ouspheaktraphal@gmail.com>
2024-02-21 23:54:58 +0700

    Add jobs

---
+++ src/jobs.ts
@@ -0,0 +1,30 @@
+type Job = (done: () => void) => void;
+
+class Jobs {
+  jobs: Job[] = [];
+  private run = (id: number) => {
+    console.log(`Run job id=${id}`)
+    this.jobs[id - 1](() => {
+      const jobsCount = this.jobs.length;
+      if (id === jobsCount) {
+        console.log(`done all ${jobsCount} jobs`);
+        this.jobs = [];
+      } else {
+        console.log(`Job ${id}/${jobsCount} done`);
+        this.run(id + 1);
+      }
+    });
+  }
+  add = (job: Job) => {
+    this.jobs.push(job);
+    const id = this.jobs.length;
+    console.log(`Job added id=${id}`);
+    if (id === 1)
+      this.run(id);
+    return this;
+  };
+}
+
+const jobs = new Jobs();
+
+export default jobs;
