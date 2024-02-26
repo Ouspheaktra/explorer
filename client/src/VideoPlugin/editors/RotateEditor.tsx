15ee2f864c7f354df74e9b0c40d77ec89d37ec09
Ouspheaktra <ouspheaktraphal@gmail.com>
2024-02-26 21:20:37 +0700

    Improve rotation

--- client/src/VideoPlugin/editors/RotateEditor.tsx
+++ client/src/VideoPlugin/editors/RotateEditor.tsx
@@ -2,15 +2,17 @@ import { EditorComponentProps } from "../../Editor/types";
 import { useGlobal } from "../../GlobalContext";
 
 const rotate = (degree: number) =>
-  `ffmpeg -display_rotation ${360 - degree} -i {input} -c copy {output}`;
+  `ffmpeg -display_rotation ${degree} -i {input} -c copy {output}`;
 
 export default function RotateRenderer({ selecteds }: EditorComponentProps) {
   const { commandFiles } = useGlobal();
   return (
     <div>
       <label className="label">rotate</label>
-      <button onClick={() => commandFiles(selecteds, rotate(90), ".mp4")}>90</button>
-      <button onClick={() => commandFiles(selecteds, rotate(270), ".mp4")}>-90</button>
+      <button onClick={() => commandFiles(selecteds, rotate(0), ".mp4")}>0</button>
+      <button onClick={() => commandFiles(selecteds, rotate(-90), ".mp4")}>90</button>
+      <button onClick={() => commandFiles(selecteds, rotate(180), ".mp4")}>180</button>
+      <button onClick={() => commandFiles(selecteds, rotate(90), ".mp4")}>-90</button>
     </div>
   );
 }
